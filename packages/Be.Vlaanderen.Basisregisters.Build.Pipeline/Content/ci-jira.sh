491a3093533efc5cc97d20d91c0aacf3e5101a73
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-12-20 18:14:27 +0100

    Delete packages/Be.Vlaanderen.Basisregisters.Build.Pipeline directory

--- packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/ci-jira.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-python packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/ci-jiraversion.py ${JIRA_PREFIX}-${JIRA_VERSION} ${JIRA_PROJECT} \
-    --user "${CONFLUENCE_USERNAME}" \
-    --password "${CONFLUENCE_PASSWORD}" \
-    --orgname "vlaamseoverheid" \
-    --github "https://github.com/Informatievlaanderen" \
-    --repo "${CONFLUENCE_TITLE}"
