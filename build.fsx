8f6491e8732273be2a0908e476865a75eb83a46b
Arne Dumarey <dumarey.arne@gmail.com>
2023-04-14 18:49:59 +0200

    fix: style to trigger build

--- build.fsx
+++ build.fsx
@@ -37,7 +37,6 @@ let push = push dockerRepository
 supportedRuntimeIdentifiers <- [ "msil"; "linux-x64" ]
 
 // Solution -----------------------------------------------------------------------
-
 Target.create "Restore_Solution" (fun _ -> restore "niscode-service")
 
 Target.create "Build_Solution" (fun _ ->
