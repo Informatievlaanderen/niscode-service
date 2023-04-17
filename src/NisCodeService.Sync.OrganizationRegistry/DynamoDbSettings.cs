6de0e36fb4045ee49bef2ee004eed92df7e0568a
pgallik <patrickgallik@gmail.com>
2023-04-17 11:23:11 +0200

    fix: remove tablename from settings

--- src/NisCodeService.Sync.OrganizationRegistry/DynamoDbSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NisCodeService.Sync.OrganizationRegistry
-{
-    public class DynamoDbSettings
-    {
-        public string TableName { get; set; } = "OVONIS";
-
-        public bool CreateTableIfNotExists { get; set; } = false;
-    }
-}
