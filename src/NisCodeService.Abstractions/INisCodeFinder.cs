f31c3929ba0b9d9650698e97515f4cb097183712
pgallik <patrickgallik@gmail.com>
2023-04-21 09:32:57 +0200

    feat: add niscodefinder & ovocodewhitlist

---
+++ src/NisCodeService.Abstractions/INisCodeFinder.cs
@@ -0,0 +1,10 @@
+﻿namespace NisCodeService.Abstractions
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface INisCodeFinder<T>
+    {
+        Task<string?> FindAsync(T id, CancellationToken ct);
+    }
+}
