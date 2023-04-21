42f78c4736510d5090f2d58152b9d509b68d547d
pgallik <patrickgallik@gmail.com>
2023-04-21 09:32:57 +0200

    feat: add niscodefinder & ovocodewhitlist

---
+++ src/NisCodeService.Abstractions/IOvoCodeWhiteList.cs
@@ -0,0 +1,26 @@
+﻿namespace NisCodeService.Abstractions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public interface IOvoCodeWhiteList
+    {
+        bool IsWhiteListed(string ovoCode);
+    }
+
+    public class OvoCodeWhiteList : IOvoCodeWhiteList
+    {
+        private readonly IList<string> _ovoCodeWhiteList;
+
+        public OvoCodeWhiteList(IList<string> ovoCodeWhiteList)
+        {
+            _ovoCodeWhiteList = ovoCodeWhiteList;
+        }
+
+        public bool IsWhiteListed(string ovoCode)
+        {
+            return _ovoCodeWhiteList.Contains(ovoCode, StringComparer.InvariantCultureIgnoreCase);
+        }
+    }
+}
